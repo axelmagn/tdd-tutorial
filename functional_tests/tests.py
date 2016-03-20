828dbaeaa57f5a2bffeaec628eabefa6dbe38056
Axel Magnuson <axelmagn@gmail.com>
2016-03-20 11:58:45 +0800

    new URL, view, and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -68,7 +68,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-        # Fancis starts a new list by entering a new item.  He is less
+        # Francis starts a new list by entering a new item.  He is less
         # interesting than Edith...
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys('Buy milk')
@@ -76,7 +76,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis gets his own unique url
         francis_list_url = self.browser.current_url
-        self.assertRegex(fancis_list_url, '/lists/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
